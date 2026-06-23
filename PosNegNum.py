eda3a8fbc41bf742e4703c97627f6495fcc9e41c
asmaacode051-star <asmaacode051@gmail.com>
2026-06-23 20:35:44 +0300

    Add files via upload

---
+++ PosNegNum.py
@@ -0,0 +1,9 @@
+number = float (input("enter a number\n"))
+
+if number > 0 :
+    print (f"{number} is positive")
+elif number < 0 :
+    print (f"{number} is negative")
+else :
+    print ("0 neither negative or positive ")
+     
