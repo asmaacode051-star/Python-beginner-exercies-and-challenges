0b89330d4bc5053beaa736c25bf0e25e37694269
asmaacode051-star <asmaacode051@gmail.com>
2026-06-23 21:29:42 +0300

    Add files via upload

---
+++ gradeScore.py
@@ -0,0 +1,11 @@
+print ("Know your evaluation ")
+score = float (input("Enter your test score \n"))
+
+if  score >= 90 :
+    print ("your grade is excllent ")
+elif  score >= 75:
+    print ("your grade is good  ")
+elif score >= 50 :
+    print ("your grade is acceptance ")
+else :
+    print ("your grade is weak ")
