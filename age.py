0145e2bd25184a764903138aa7b5e4642ed6c191
asmaacode051-star <asmaacode051@gmail.com>
2026-06-23 20:23:40 +0300

    Add files via upload

---
+++ age.py
@@ -0,0 +1,7 @@
+print ("Welcome to my application")
+user = int (input ("How old are you ?\n") )
+
+if user < 12:
+    print ("sorry, you can not use this app ")
+else :
+    print ("Good. you can use this app ")
