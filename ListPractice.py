ddc2a57fec798d0019c1298738654c8eb84dc102
asmaacode051-star <asmaacode051@gmail.com>
2026-09-05 04:11:04 +0300

    Add files via upload

---
+++ ListPractice.py
@@ -0,0 +1,12 @@
+# First
+friends = ["Sam","Louis","Alan"]
+print (friends)
+
+# Second
+Countries = ["Canda","Iraq","Germany","China"] 
+Countries [1] = "Egypt"
+print (Countries)
+
+# Third
+nums = [2,4,6,8,10,12]
+print (f"the first number is {nums[0]} and last number is {nums[-1]}")
