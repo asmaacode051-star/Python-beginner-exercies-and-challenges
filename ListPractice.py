a27da3cecedb3ed15befc048115e4487deddbbcd
asmaacode051-star <asmaacode051@gmail.com>
2026-09-05 05:58:03 +0300

    Update ListPractice.py

--- ListPractice.py
+++ ListPractice.py
@@ -11,3 +11,17 @@ print (Countries)
 # Third
 nums = [2,4,6,8,10,12]
 print (f"the first number is {nums[0]} and last number is {nums[-1]}")
+
+# Fourth
+Class_a = ["Tom","Peter","Roy"]
+Class_b = ["Fady","John","Chan"]
+all = []
+all.extend(Class_a)
+all.extend(Class_b)
+print(all)
+
+# Fifth
+Class_c = ["Tom","Peter","Roy"]
+Class_d = ["Fady","John","Chan"]
+all_students = Class_c + Class_d
+print (all_students)
